639a6171e319191d16bbd56ca285257147a88b36
petrk94 <petrk94@users.noreply.github.com>
2017-03-02 23:10:36 +0100

    added comments to code

--- humhub_autoinstaller.sh
+++ humhub_autoinstaller.sh
@@ -1,9 +1,21 @@
+# git clone repository to server
 git clone https://github.com/humhub/humhub.git
+
+# change directory to new created humhub folder
 cd humhub
+
+# checkout the stable branch
 git checkout stable
+
+# download composer installer with wget
 wget https://getcomposer.org/installer
+
+# run the composer installer with php
 php installer
+
+# run th composer.phar file
 php composer.phar global require "fxp/composer-asset-plugin:~1.1.1"
+
 echo
 echo PLEASE FOLLOW THE INSTRUCTIONS BELOW
 echo
